6755b82e48ffbe63e83d5e667b40f885816b9fc5
Prathmesh <99099366+PrathmeshRanjan@users.noreply.github.com>
2025-10-15 17:18:28 +0530

    Cleanup unused imports in SendPackedUserOp.s.sol
    Removed unused imports from SendPackedUserOp contract.

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -6,9 +6,6 @@ import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract SendPackedUserOp is Script {
     using MessageHashUtils for bytes32;
